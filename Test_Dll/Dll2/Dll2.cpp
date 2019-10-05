d3df97c0c443716e2198afe8ad1d24ef4eeddc24
NoEureka <315621774@qq.com>
2019-10-05 20:06:03 +0800

    测试c++动态链接库

---
+++ Test_Dll/Dll2/Dll2.cpp
@@ -0,0 +1,24 @@
+﻿// Dll2.cpp : 定义 DLL 应用程序的导出函数。
+//
+
+#include "stdafx.h"
+#include "first.h"
+#include <Windows.h>
+#include <iostream>
+using namespace std;
+void show(void)
+{
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED);
+	cout << "                    加";
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN);
+	cout << "油";
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_GREEN | FOREGROUND_BLUE);
+	cout << ",";
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_BLUE);
+	cout << "给";
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_GREEN);
+	cout << "自";
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_BLUE);
+	cout << "己";
+}
+
